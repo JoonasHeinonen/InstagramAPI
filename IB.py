1bc545775f0f540e7a9eb102f67b15144393cf2e
Joonas Heinonen <joonas.heinonen@edu.turkuamk.fi>
2019-05-19 23:16:33 +0300

    Added path to find elements.

--- IB.py
+++ IB.py
@@ -18,9 +18,19 @@ class InstagramBot:
         driver = self.driver
         driver.get("https://www.instagram.com")
         time.sleep(2)
-        
+        login_button = driver.find_element_by_xpath("//a[@href='/accounts/login/?source=auth_switcher']")
+        login_button.click()
+        time.sleep(2)
+        username_elem = driver.find_element_by_xpath("//input[@name='username']")
+        username_elem.clear()
+        username_elem.send_keys(self.username)
+        password_elem = driver.find_element_by_xpath("//input[@name='password']")
+        password_elem.clear()
+        password_elem.send_keys(self.password)
+        password_elem.send_keys(Keys.RETURN)
+
         # "//a[@href'accounts/login']"
-        # "//input[@name='username']"
+        # "//input[@name='username']"Kirjaudu sisään<a href="/accounts/login/?source=auth_switcher">Kirjaudu sisään</a>
         # "//input[@name='password']"
 
 girdeuxIG = InstagramBot("Heinonen", "Password12345")
