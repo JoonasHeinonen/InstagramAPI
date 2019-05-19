64c8d626a7f736e18c655698191127d05233262f
Joonas Heinonen <joonas.heinonen@edu.turkuamk.fi>
2019-05-19 19:45:09 +0300

    Created the class and an object.

--- IB.py
+++ IB.py
@@ -2,3 +2,26 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 
+class InstagramBot:
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        self.driver = webdriver.Firefox()
+
+    def closeBrowser(self):
+        self.driver.close()
+
+    def login(self):
+        driver = self.driver
+        driver.get("https://www.instagram.com")
+        time.sleep(2)
+
+
+        # "<a href="/accounts/login/?source=auth_switcher">Kirjaudu sisään</a>"
+        # "//a[@href'accounts/login']"
+        # "//input[@name='username']"
+        # "//input[@name='password']"
+
+girdeuxIG = InstagramBot("Heinonen", "Password12345")
+girdeuxIG.login()
