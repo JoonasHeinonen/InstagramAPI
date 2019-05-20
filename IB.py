19643bd0f5525c798f41966543a8b13a0d57fd92
Joonas Heinonen <joonas.heinonen@edu.turkuamk.fi>
2019-05-20 21:13:19 +0300

    Instagram bot's code copied from source code.

--- IB.py
+++ IB.py
@@ -1,8 +1,16 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
+import random
+import sys
 
 #https://stackoverflow.com/questions/40208051/selenium-using-python-geckodriver-executable-needs-to-be-in-path
+
+def print_same_line(text):
+    sys.stdout.write('\r')
+    sys.stdout.flush()
+    sys.stdout.write(text)
+    sys.stdout.flush()
 
 class InstagramBot:
 
@@ -28,10 +36,64 @@ class InstagramBot:
         password_elem.clear()
         password_elem.send_keys(self.password)
         password_elem.send_keys(Keys.RETURN)
+        time.sleep(2)
 
-        # "//a[@href'accounts/login']"
-        # "//input[@name='username']"Kirjaudu sisään<a href="/accounts/login/?source=auth_switcher">Kirjaudu sisään</a>
-        # "//input[@name='password']"
+    def like_photo(self, hashtag):
+        driver = self.driver
+        driver.get("https://www.instagram.com/explore/tags/" + hashtag + "/")
+        time.sleep(2)
 
-girdeuxIG = InstagramBot("Heinonen", "Password12345")
-girdeuxIG.login()
+        # gathering photos
+        pic_hrefs = []
+        for i in range(1, 7):
+            try:
+                driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+                time.sleep(2)
+                # get tags
+                hrefs_in_view = driver.find_elements_by_tag_name('a')
+                # finding relevant hrefs
+                hrefs_in_view = [elem.get_attribute('href') for elem in hrefs_in_view
+                                 if '.com/p/' in elem.get_attribute('href')]
+                # building list of unique photos
+                [pic_hrefs.append(href) for href in hrefs_in_view if href not in pic_hrefs]
+                # print("Check: pic href length " + str(len(pic_hrefs)))
+            except Exception:
+                continue
+
+        # Liking photos
+        unique_photos = len(pic_hrefs)
+        for pic_href in pic_hrefs:
+            driver.get(pic_href)
+            time.sleep(2)
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            try:
+                time.sleep(random.randint(2, 4))
+                like_button = lambda: driver.find_element_by_xpath('//span[@aria-label="Like"]').click()
+                like_button().click()
+                for second in reversed(range(0, random.randint(18, 28))):
+                    print_same_line("#" + hashtag + ': unique photos left: ' + str(unique_photos)
+                                    + " | Sleeping " + str(second))
+                    time.sleep(1)
+            except Exception as e:
+                time.sleep(2)
+            unique_photos -= 1
+
+if __name__ == "__main__":
+    username = "j.girdeux"
+    password = "Password12345"
+
+    girdeuxIG = InstagramBot(username, password)
+    girdeuxIG.login()
+
+    hashtags = ['amazing', 'beautiful', 'adventure']
+
+    while True:
+        try:
+            # Choose a random tag from the list of tags
+            tag = random.choice(hashtags)
+            girdeuxIG.like_photo(tag)
+        except Exception:
+            girdeuxIG.closeBrowser()
+            time.sleep(60)
+            girdeuxIG = InstagramBot(username, password)
+            girdeuxIG.login()
